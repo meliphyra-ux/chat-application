1f936fb0d049fcf869fd20aaf04b56966f28e867
meliphyra-ux <xennerit@icloud.com>
2022-09-15 19:08:17 +0300

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,67 +1,113 @@
-import './App.css';
+import "./App.css";
 
+import { initializeApp } from "firebase/app";
+import {
+  addDoc,
+  collection,
+  getFirestore,
+  limit,
+  orderBy,
+  query,
+  serverTimestamp,
+} from "firebase/firestore";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
-import { initializeApp } from 'firebase/app'
-import { collection, getFirestore, limit, orderBy, query } from 'firebase/firestore'
-import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'  
-
-import { useAuthState } from 'react-firebase-hooks/auth'
-import { useCollectionData } from 'react-firebase-hooks/firestore'
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { useRef, useState } from "react";
 
 const firebaseConfig = {
   apiKey: "AIzaSyClDrUCtiy6QSU73Rl7e4vD4NKXRavA9cI",
   authDomain: "chat-application-cba7b.firebaseapp.com",
-  databaseURL: "https://chat-application-cba7b-default-rtdb.europe-west1.firebasedatabase.app",
+  databaseURL:
+    "https://chat-application-cba7b-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "chat-application-cba7b",
   storageBucket: "chat-application-cba7b.appspot.com",
   messagingSenderId: "640857833800",
   appId: "1:640857833800:web:90e9b594bd7e6a46a4f37d",
-  measurementId: "G-S0QM18J44Q"
+  measurementId: "G-S0QM18J44Q",
 };
 
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const db = getFirestore(app);
 
 function App() {
   const [user] = useAuthState(auth);
-  return (
-    <div className="App">
-      {user ? <ChatRoom /> : <SingIn />}
+  return <div className="App">
+    <header>
+        <h1>Chat App</h1>
+        <SignOut />
+      </header>
+    <section>
+    {user ? <ChatRoom /> : <SingIn />}
+    </section>
     </div>
-  );
 }
 
-function SingIn(){
-  const signInWithGoogle = () =>{
+function SingIn() {
+  const signInWithGoogle = () => {
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider);
-  }
-  return <button onClick={signInWithGoogle}>Sign In With Google</button>
+  };
+  return <button onClick={signInWithGoogle}>Sign In With Google</button>;
 }
-function SignOut(){
-  return auth.currentUser && (
-    <button onClick={()=> auth.signOut()}>Sign out</button>
-  )
+function SignOut() {
+  return (
+    auth.currentUser && <button onClick={() => auth.signOut()}>Sign out</button>
+  );
 }
-function ChatRoom(){
-  
-  const messagesRef = collection(db,'messages');
+function ChatRoom() {
+  const messagesRef = collection(db, "messages");
   const q = query(messagesRef, orderBy("createdAt"), limit(25));
-  
-  const [messages, loading, error, snapshot] = useCollectionData(q);
-  console.log(loading, error)
+
+  const [messages, , , snapshot] = useCollectionData(q);
+
+  const [formValue, setFormValue] = useState('')
+
+  const dummy = useRef();
+
+  const sendMessage = async(e) =>{
+    e.preventDefault();
+    const {uid, photoURL} = auth.currentUser;
+
+    await addDoc(collection(db,"messages"), {
+      text: `${formValue}`,
+      createdAt: serverTimestamp(),
+      uid,
+      photoURL
+    });
+
+    setFormValue('');
+    dummy.current.scrollIntoView({behavior: 'smooth'});
+  }
   return (
     <>
-      <div>
-        {messages && messages.map((msg, index) => <ChatMessage key={snapshot.docs[index].id} message={msg}/>)}
-      </div>
-    </>
-  )
+      <main>
+        {messages &&
+          messages.map((msg, index) => (
+            <ChatMessage key={snapshot.docs[index].id} message={msg} />
+          ))}
 
-}
-function ChatMessage({message}){
+            <div ref={dummy}></div>
+
+      </main>
   
-  return <p>{message.text}</p>
+      <form onSubmit={sendMessage}>
+        <input value={formValue} onChange={e=> setFormValue(e.target.value)}/>
+        <button type="submit">Send message</button>
+      </form>
+    </>
+  );
+}
+function ChatMessage({ message }) {
+  const { text, uid, photoURL } = message;
+  const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
+  return(
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL} alt="drop"/>
+      <p>{text}</p>
+    </div>
+  )
 }
 export default App;
